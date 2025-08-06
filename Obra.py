6e8571734e6d7b63a07d11801a3df71148b53d2d
Emmanuel <197514045+zmanu26@users.noreply.github.com>
2025-08-05 22:56:03 -0400

    Update Obra.py

---
+++ Obra.py
@@ -0,0 +1,11 @@
+class Obra:
+    def __init__(self, titulo, departamento, autor, clasificacion, anio, imagen):
+        self.titulo = titulo
+        self.departamento = departamento
+        self.autor = autor
+        self.clasificacion = clasificacion
+        self.anio = anio
+        self.imagen = imagen
+    
+    def show(self):
+        pass
